004f5ffb547d498c007cd3b5156831d4b458dddd
151273 <151273@csgaugustinus.nl>
2019-12-17 13:17:13 +0000

    hoiiii

--- JS/H1/H1O30.js
+++ JS/H1/H1O30.js
@@ -1,14 +1,21 @@
-var rij = 9;
+var rij = 0;
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(450,450);
   canvas.parent('processing');
   background('coral');
   fill('white');
 }
 
 function draw() {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      rect(kolom,rij,50,50);
+    for (var rij = 0;rij < 450;rij += 50) {   
+        for (var kolom = 0;kolom < 450;kolom += 50) {
+             rect(kolom,rij,50,50);
+        }   
+
+        if (rij == 150 && kolom == 300) {
+            fill('orange');
+        }
+
     }
 }
